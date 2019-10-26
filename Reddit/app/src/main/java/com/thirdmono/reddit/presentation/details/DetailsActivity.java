675753181fd188c1859008ccac20e875afd07245
Victor Maldonado <vmmzn20@gmail.com>
2019-10-26 18:51:11 -0500

    Replacing Butterknife with viewbinding for DetailsActivity.

--- Reddit/app/src/main/java/com/thirdmono/reddit/presentation/details/DetailsActivity.java
+++ Reddit/app/src/main/java/com/thirdmono/reddit/presentation/details/DetailsActivity.java
@@ -3,80 +3,34 @@ package com.thirdmono.reddit.presentation.details;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.text.Spanned;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.mukesh.MarkdownView;
 import com.squareup.picasso.Picasso;
 import com.thirdmono.reddit.R;
 import com.thirdmono.reddit.data.entity.SubReddit;
 import com.thirdmono.reddit.data.entity.Thing;
+import com.thirdmono.reddit.databinding.ActivityDetailsBinding;
 import com.thirdmono.reddit.domain.utils.Constants;
 import com.thirdmono.reddit.domain.utils.Utils;
 import com.thirdmono.reddit.presentation.BaseActivity;
 
 import java.util.Locale;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import de.hdodenhof.circleimageview.CircleImageView;
 import timber.log.Timber;
 
 public class DetailsActivity extends BaseActivity {
 
-    @BindView(R.id.toolbar_layout)
-    CollapsingToolbarLayout collapsingToolbarLayout;
-
-    @BindView(R.id.app_bar)
-    AppBarLayout appBarLayout;
-
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-
-    @BindView(R.id.subreddit_banner)
-    ImageView banner;
-
-    @BindView(R.id.subreddit_icon)
-    CircleImageView icon;
-
-    @BindView(R.id.subreddit_name)
-    TextView name;
-
-    @BindView(R.id.subreddit_subscribers)
-    TextView subscribers;
-
-    @BindView(R.id.subreddit_public_description)
-    TextView publicDescription;
-
-    @BindView(R.id.subreddit_full_description)
-    MarkdownView fullDescription;
-
     String title;
     Thing thing;
     SubReddit subReddit;
-
-    @SuppressWarnings("deprecation")
-    public static Spanned fromHtml(String html) {
-        Spanned result;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
-        } else {
-            result = Html.fromHtml(html);
-        }
-        return result;
-    }
+    ActivityDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
-        ButterKnife.bind(this);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setupToolbar();
 
@@ -110,20 +64,20 @@ public class DetailsActivity extends BaseActivity {
 
     private void setupDetailHeader() {
         title = subReddit.getDisplayName();
-        name.setText(String.format("r/%s", subReddit.getDisplayName()));
-        subscribers.setText(String.format(Locale.getDefault(), "%d Subscribers", subReddit.getSubscribers()));
+        binding.subredditName.setText(String.format("r/%s", subReddit.getDisplayName()));
+        binding.subredditSubscribers.setText(String.format(Locale.getDefault(), "%d Subscribers", subReddit.getSubscribers()));
 
         if (subReddit.getBannerImg() != null && !subReddit.getBannerImg().isEmpty()) {
             Picasso.with(this)
                     .load(subReddit.getBannerImg())
                     .fit()
                     .centerCrop()
-                    .into(banner);
+                    .into(binding.subredditBanner);
         }
 
         if (subReddit.getKeyColor() != null && !subReddit.getKeyColor().isEmpty()) {
-            toolbar.setBackgroundColor(Color.parseColor(subReddit.getKeyColor()));
-            banner.setBackgroundColor(Color.parseColor(subReddit.getKeyColor()));
+            binding.toolbar.setBackgroundColor(Color.parseColor(subReddit.getKeyColor()));
+            binding.subredditBanner.setBackgroundColor(Color.parseColor(subReddit.getKeyColor()));
         }
 
         if (subReddit.getIconImg() != null && !subReddit.getIconImg().trim().isEmpty()) {
@@ -131,30 +85,30 @@ public class DetailsActivity extends BaseActivity {
                     .load(subReddit.getIconImg())
                     .fit()
                     .centerCrop()
-                    .into(icon);
+                    .into(binding.subredditIcon);
         }
     }
 
     private void setupDetailBody() {
-        publicDescription.setText(subReddit.getPublicDescription());
-        fullDescription.setMarkDownText(subReddit.getDescription());
-        fullDescription.setOpenUrlInBrowser(true);
+        binding.content.subredditPublicDescription.setText(subReddit.getPublicDescription());
+        binding.content.subredditFullDescription.setMarkDownText(subReddit.getDescription());
+        binding.content.subredditFullDescription.setOpenUrlInBrowser(true);
     }
 
     private void setupToolbar() {
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_action_back);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setNavigationIcon(R.drawable.ic_action_back);
+        binding.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
 
-        collapsingToolbarLayout.setTitle(" ");
-        collapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, android.R.color.transparent));
+        binding.toolbarLayout.setTitle(" ");
+        binding.toolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, android.R.color.transparent));
 
-        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+        binding.appBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             boolean isShow = false;
             int scrollRange = -1;
 
@@ -164,10 +118,10 @@ public class DetailsActivity extends BaseActivity {
                     scrollRange = appBarLayout.getTotalScrollRange();
                 }
                 if (scrollRange + verticalOffset == 0) {
-                    collapsingToolbarLayout.setTitle(title);
+                    binding.toolbarLayout.setTitle(title);
                     isShow = true;
                 } else if (isShow) {
-                    collapsingToolbarLayout.setTitle(" ");
+                    binding.toolbarLayout.setTitle(" ");
                     isShow = false;
                 }
             }
